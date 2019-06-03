478e649987b50b848592b2e41e650ad5d0deefb5
Ganeswararao Gara (HCL America Inc) <v-gagara@microsoft.com>
2019-06-03 19:31:45 +0530

    first commit

---
+++ src/conduentdemo/controllers/HomePageController.java
@@ -0,0 +1,5 @@
+package conduentdemo.controllers;
+
+public class HomePageController {
+
+}
---
+++ src/conduentdemo/controllers/HomePageServlet.java
@@ -0,0 +1,21 @@
+package conduentdemo.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomePageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public HomePageServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
+	}
+
+}
