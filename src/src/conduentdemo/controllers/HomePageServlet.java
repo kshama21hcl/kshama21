91b7ea3ee39fb161e607bfc02dbfa945362bae6d
Keerti Rastogi <KeertiR@hcl.com>
2019-06-13 13:24:17 +0530

    Downloaded code

---
+++ src/src/conduentdemo/controllers/HomePageServlet.java
@@ -0,0 +1,21 @@
+package src.conduentdemo.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomePageServlet extends HttpServlet {
+	private static final long serialVersionUID = 102831973239L;
+    public HomePageServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
+	}
+
+}
